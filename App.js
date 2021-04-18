0541154a6d9533ff5c4ebc83906e8ceae4f984fc
jeongmin <luvmini511@gmail.com>
2021-04-18 17:40:04 +0900

    update: 현재 위치 습득

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Alert } from "react-native";
 import { Loading } from "./Loading"
+import * as Location from 'expo-location';
+
+const getLocation = async () => {
+  try {
+    await Location.requestForegroundPermissionsAsync()
+    const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync()
+    console.log(latitude, longitude)
+  } catch (error) {
+    Alert.alert("현재 위치를 가져올 수 없습니다")
+  }
+}
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    getLocation()
+  }, [])
+
   return (
-      <Loading />
+      isLoading && <Loading />
   );
 }
